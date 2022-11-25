d33cc88eadad57e39a23c61dc99d2a5c6e4a1762
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-11-25 19:46:46 +0800

    ver: bump version to 0.4.1

--- optree/version.py
+++ optree/version.py
@@ -14,7 +14,7 @@
 # ==============================================================================
 """OpTree: Optimized PyTree Utilities."""
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 __license__ = 'Apache License, Version 2.0'
 __author__ = 'OpTree Contributors'
 __release__ = False
