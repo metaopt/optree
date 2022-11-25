92d91ce2538641e90688fa5c5ad26f251a38c651
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-11-25 19:02:39 +0800

    feat(workflows): enable PyPy in CI

--- .github/workflows/set_cibw_build.py
+++ .github/workflows/set_cibw_build.py
@@ -1,9 +1,12 @@
 import os
-import sys
+import platform
 
 
 # pylint: disable-next=consider-using-f-string
-CIBW_BUILD = 'CIBW_BUILD=*cp%d%d-*' % sys.version_info[:2]
+CIBW_BUILD = 'CIBW_BUILD=*%sp%s%s-*' % (
+    platform.python_implementation().lower()[0],
+    *platform.python_version_tuple()[:2],
+)
 
 print(CIBW_BUILD)
 with open(os.getenv('GITHUB_ENV'), mode='at', encoding='UTF-8') as file:
