b3aa92b5b50692858cdecd1a092436d7064feb1d
Xuehai Pan <XuehaiPan@pku.edu.cn>
2023-03-06 07:36:59 +0000

    chore: add sanity check for `GetKind`

--- src/treespec/flatten.cpp
+++ src/treespec/flatten.cpp
@@ -51,7 +51,7 @@ bool PyTreeSpec::FlattenIntoImpl(const py::handle& handle,
                 if (!NoneIsLeaf) {
                     break;
                 }
-                [[fallthrough]];
+                INTERNAL_ERROR("NoneIsLeaf is true, but `GetKind` returned `PyTreeKind::None`.");
             case PyTreeKind::Leaf:
                 leaves.emplace_back(py::reinterpret_borrow<py::object>(handle));
                 break;
@@ -231,7 +231,7 @@ bool PyTreeSpec::FlattenIntoWithPathImpl(const py::handle& handle,
                 if (!NoneIsLeaf) {
                     break;
                 }
-                [[fallthrough]];
+                INTERNAL_ERROR("NoneIsLeaf is true, but `GetKind` returned `PyTreeKind::None`.");
             case PyTreeKind::Leaf: {
                 py::tuple path{depth};
                 for (ssize_t d = 0; d < depth; ++d) {
