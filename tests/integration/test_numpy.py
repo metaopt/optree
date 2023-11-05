48722f44cc6c782290e9107bbfc84b2d504b3485
Xuehai Pan <XuehaiPan@pku.edu.cn>
2023-11-05 23:16:26 +0800

    test(integration/numpy): add tests for NumPy integration

--- tests/integration/test_numpy.py
+++ tests/integration/test_numpy.py
@@ -18,15 +18,11 @@
 import pytest
 
 
-jax = pytest.importorskip('jax')
+numpy = pytest.importorskip('numpy')
 
 import random
 
-import jax
-import jax.numpy as jnp
 import numpy as np
-from jax import lax
-from jax._src import dtypes
 
 import optree
 from helpers import LEAVES, TREES, parametrize
@@ -34,7 +30,6 @@ from helpers import LEAVES, TREES, parametrize
 
 @parametrize(tree=list(TREES + LEAVES))
 def test_tree_ravel(tree):
-    jax.config.update('jax_enable_x64', True)
     random.seed(0)
 
     def replace_leaf(_):
@@ -47,52 +42,51 @@ def test_tree_ravel(tree):
             (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
         ]
         dtypes = [
-            jnp.float32,
-            jnp.float64,
-            jnp.int32,
-            jnp.int64,
+            np.float32,
+            np.float64,
+            np.int32,
+            np.int64,
         ]
         for dtype in dtypes:
             candidates.extend(
-                jnp.array(np.random.uniform(low=-5.0, high=5.0, size=shape), dtype=dtype)
-                for shape in shapes
+                np.random.uniform(low=-5.0, high=5.0, size=shape).astype(dtype) for shape in shapes
             )
 
         return random.choice(candidates)
 
     tree = optree.tree_map(replace_leaf, tree)
-    flat, unravel_func = optree.integration.jax.tree_ravel(tree)
+    flat, unravel_func = optree.integration.numpy.tree_ravel(tree)
 
     leaves, treespec = optree.tree_flatten(tree)
-    assert jnp.size(flat) == sum(jnp.size(leaf) for leaf in leaves)
-    assert jnp.shape(flat) == (jnp.size(flat),)
+    assert np.size(flat) == sum(np.size(leaf) for leaf in leaves)
+    assert np.shape(flat) == (np.size(flat),)
 
     reconstructed = unravel_func(flat)
     reconstructed_leaves, reconstructed_treespec = optree.tree_flatten(reconstructed)
     assert reconstructed_treespec == treespec
     assert len(leaves) == len(reconstructed_leaves)
     for leaf, reconstructed_leaf in zip(leaves, reconstructed_leaves):
-        assert jnp.allclose(leaf, reconstructed_leaf)
-        leaf = jnp.asarray(leaf)
-        reconstructed_leaf = jnp.asarray(reconstructed_leaf)
+        assert np.allclose(leaf, reconstructed_leaf)
+        leaf = np.asarray(leaf)
+        reconstructed_leaf = np.asarray(reconstructed_leaf)
         assert leaf.dtype == reconstructed_leaf.dtype
         assert leaf.shape == reconstructed_leaf.shape
 
-    if all(dtypes.dtype(leaf) == dtypes.dtype(flat) for leaf in leaves):
-        unravel_func(lax.convert_element_type(flat, jnp.complex128))
+    if all(np.result_type(leaf) == flat.dtype for leaf in leaves):
+        unravel_func(flat.astype(np.complex128))
     else:
         with pytest.raises(
             ValueError,
             match=r'The unravel function given array of dtype .*, but expected dtype .*\.',
         ):
-            unravel_func(lax.convert_element_type(flat, jnp.complex128))
+            unravel_func(flat.astype(np.complex128))
 
 
 @parametrize(tree=list(TREES + LEAVES))
 def test_tree_ravel_single_dtype(tree):
-    jax.config.update('jax_enable_x64', True)
     random.seed(0)
-    jax_dtype = jnp.float32
+    dtype = np.float16
+    default_dtype = np.array([]).dtype
 
     def replace_leaf(_):
         candidates = []
@@ -103,28 +97,27 @@ def test_tree_ravel_single_dtype(tree):
             (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
         ]
         candidates.extend(
-            jnp.array(np.random.uniform(low=-5.0, high=5.0, size=shape), dtype=jax_dtype)
-            for shape in shapes
+            np.random.uniform(low=-5.0, high=5.0, size=shape).astype(dtype) for shape in shapes
         )
         return random.choice(candidates)
 
     tree = optree.tree_map(replace_leaf, tree)
-    flat, unravel_func = optree.integration.jax.tree_ravel(tree)
+    flat, unravel_func = optree.integration.numpy.tree_ravel(tree)
 
     leaves, treespec = optree.tree_flatten(tree)
-    assert flat.dtype == jax_dtype if leaves else jnp.float64
-    assert jnp.size(flat) == sum(jnp.size(leaf) for leaf in leaves)
-    assert jnp.shape(flat) == (jnp.size(flat),)
+    assert flat.dtype == dtype if leaves else default_dtype
+    assert np.size(flat) == sum(np.size(leaf) for leaf in leaves)
+    assert np.shape(flat) == (np.size(flat),)
 
     reconstructed = unravel_func(flat)
     reconstructed_leaves, reconstructed_treespec = optree.tree_flatten(reconstructed)
     assert reconstructed_treespec == treespec
     assert len(leaves) == len(reconstructed_leaves)
     for leaf, reconstructed_leaf in zip(leaves, reconstructed_leaves):
-        assert jnp.allclose(leaf, reconstructed_leaf)
-        leaf = jnp.asarray(leaf)
-        reconstructed_leaf = jnp.asarray(reconstructed_leaf)
+        assert np.allclose(leaf, reconstructed_leaf)
+        leaf = np.asarray(leaf)
+        reconstructed_leaf = np.asarray(reconstructed_leaf)
         assert leaf.dtype == reconstructed_leaf.dtype
         assert leaf.shape == reconstructed_leaf.shape
 
-    unravel_func(flat.astype(jnp.complex128))
+    unravel_func(flat.astype(np.complex128))
