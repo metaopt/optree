36fb06031a2635b923e50dcf2532dc0f66b3c926
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-07-23 14:17:05 +0800

    deps(python): enable Python 3.13t support

--- .github/workflows/set_cibw_build.py
+++ .github/workflows/set_cibw_build.py
@@ -7,7 +7,8 @@ import platform
 
 
 MAJOR, MINOR, *_ = platform.python_version_tuple()
-CIBW_BUILD = f'CIBW_BUILD=*{platform.python_implementation().lower()[0]}p{MAJOR}{MINOR}-*'
+IMPLEMENTATION = platform.python_implementation()
+CIBW_BUILD = f'CIBW_BUILD=*{IMPLEMENTATION.lower()[0]}p{MAJOR}{MINOR}{{,?}}-*'
 
 print(CIBW_BUILD)
 with open(os.getenv('GITHUB_ENV'), mode='a', encoding='utf-8') as file:
