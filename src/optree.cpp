e7446eed153de0032160448778368c4a5e37b984
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-08-02 13:55:03 +0800

    feat!: use C++20 for extension build

--- src/optree.cpp
+++ src/optree.cpp
@@ -285,10 +285,8 @@ void BuildModule(py::module_ &mod) {  // NOLINT[runtime/references]
 #endif
     auto * const PyTreeKind_Type = reinterpret_cast<PyTypeObject *>(PyTreeKindTypeObject.ptr());
     PyTreeKind_Type->tp_name = "optree.PyTreeKind";
-    py::setattr(PyTreeKindTypeObject.ptr(), Py_Get_ID(__module__), Py_Get_ID(optree));
-    py::setattr(PyTreeKindTypeObject.ptr(),
-                "NUM_KINDS",
-                py::int_(py::ssize_t(PyTreeKind::NumKinds)));
+    py::setattr(PyTreeKindTypeObject, Py_Get_ID(__module__), Py_Get_ID(optree));
+    py::setattr(PyTreeKindTypeObject, "NUM_KINDS", py::int_(py::ssize_t(PyTreeKind::NumKinds)));
 
     auto PyTreeSpecTypeObject =
 #if defined(PYBIND11_HAS_INTERNALS_WITH_SMART_HOLDER_SUPPORT)
@@ -311,7 +309,7 @@ void BuildModule(py::module_ &mod) {  // NOLINT[runtime/references]
             py::module_local());
     auto * const PyTreeSpec_Type = reinterpret_cast<PyTypeObject *>(PyTreeSpecTypeObject.ptr());
     PyTreeSpec_Type->tp_name = "optree.PyTreeSpec";
-    py::setattr(PyTreeSpecTypeObject.ptr(), Py_Get_ID(__module__), Py_Get_ID(optree));
+    py::setattr(PyTreeSpecTypeObject, Py_Get_ID(__module__), Py_Get_ID(optree));
 
     PyTreeSpecTypeObject
         .def("unflatten",
@@ -509,7 +507,7 @@ void BuildModule(py::module_ &mod) {  // NOLINT[runtime/references]
             py::module_local());
     auto * const PyTreeIter_Type = reinterpret_cast<PyTypeObject *>(PyTreeIterTypeObject.ptr());
     PyTreeIter_Type->tp_name = "optree.PyTreeIter";
-    py::setattr(PyTreeIterTypeObject.ptr(), Py_Get_ID(__module__), Py_Get_ID(optree));
+    py::setattr(PyTreeIterTypeObject, Py_Get_ID(__module__), Py_Get_ID(optree));
 
     PyTreeIterTypeObject
         .def(py::init<py::object, std::optional<py::function>, bool, std::string>(),
