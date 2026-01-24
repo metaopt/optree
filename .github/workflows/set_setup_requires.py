98fec43beb279cf2868e8349f2d97705f48f2b8b
Xuehai Pan <XuehaiPan@pku.edu.cn>
2026-01-24 23:00:04 +0800

    chore: set remote packages

--- .github/workflows/set_setup_requires.py
+++ .github/workflows/set_setup_requires.py
@@ -10,15 +10,17 @@ ROOT = Path(__file__).absolute().parents[2]
 
 PYPROJECT_FILE = ROOT / 'pyproject.toml'
 PYBIND11_GIT_URL = 'https://github.com/XuehaiPan/pybind11.git@fix-segfault'
+PYBIND11_PACKAGE = f'pybind11 @ git+{PYBIND11_GIT_URL}#egg=pybind11'
 
 
 if __name__ == '__main__':
+    print(PYBIND11_PACKAGE)
     PYPROJECT_CONTENT = PYPROJECT_FILE.read_text(encoding='utf-8')
 
     PYPROJECT_FILE.write_text(
         data=re.sub(
             r'(requires\s*=\s*\[.*"\s*)\bpybind11\b[^"]*(\s*".*\])',
-            rf'\g<1>pybind11 @ git+{PYBIND11_GIT_URL}#egg=pybind11\g<2>',
+            rf'\g<1>{PYBIND11_PACKAGE}\g<2>',
             string=PYPROJECT_CONTENT,
         ),
         encoding='utf-8',
